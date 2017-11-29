b7d2398b0215829a671c8899e354af0df8932f9d
Nachiappan <nachiapan@gmail.com>
2017-11-29 20:43:29 +0530

    Implemented LogOut in the UI

---
+++ SafeBySimplify/SafeViewModel/EmptyOperationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using SafeModel;
+
+namespace SafeViewModel
+{
+    public class EmptyOperationViewModel : SingleOperationViewModel
+    {
+        public EmptyOperationViewModel(RecordHeader recordHeader)
+        {
+            HighlightCommand = new DelegateCommand(() =>
+            {
+                
+            });
+        }
+    }
+}
--- SafeBySimplify/SafeViewModel/SingleOperationViewModel.cs
+++ SafeBySimplify/SafeViewModel/SingleOperationViewModel.cs
@@ -5,25 +5,12 @@ namespace SafeViewModel
 {
     public class SingleOperationViewModel
     {
-        public SingleOperationViewModel(RecordHeader recordHeader)
-        {
-
-        }
-
         public DelegateCommand HighlightCommand { get; set; }
-    }
-
-    public class EmptyOperationViewModel : SingleOperationViewModel
-    {
-        public EmptyOperationViewModel(RecordHeader recordHeader) : base(recordHeader)
-        {
-            HighlightCommand = new DelegateCommand(() => { });
-        }
     }
 
     public class RecordAlteringOperationViewModel : SingleOperationViewModel
     {
-        public RecordAlteringOperationViewModel(RecordHeader recordHeader) : base(recordHeader)
+        public RecordAlteringOperationViewModel(RecordHeader recordHeader)
         {
         }
     }
