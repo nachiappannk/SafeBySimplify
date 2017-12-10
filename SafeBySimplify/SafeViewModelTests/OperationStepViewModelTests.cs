d042c288224187d1b7419fe1be170da0ba40fd31
Nachiappan <nachiapan@gmail.com>
2017-12-10 16:48:02 +0530

    Cleaned up Operation Step View Model Tests And Implemented to check if redundant search calls are not made

--- SafeBySimplify/SafeViewModelTests/OperationStepViewModelTests.cs
+++ SafeBySimplify/SafeViewModelTests/OperationStepViewModelTests.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 using SafeModel;
 using SafeViewModel;
@@ -47,40 +48,80 @@ namespace SafeViewModelTests
             _operationChangingPossibilityObserver = _operationStepViewModel
                 .GetPropertyObserver<bool>
                 (nameof(_operationStepViewModel.IsOperationsChangingPossible));
-        }
 
-        [Test]
-        public void Search_result_is_initially_not_available()
-        {
-            Assert.AreEqual(false, _operationStepViewModel.IsSearchResultVisible);
+
+            _safe.GetRecordsAsync(Arg.Is<string>(x => string.IsNullOrEmpty(x)), Arg.Any<CancellationToken>())
+                .Throws(new Exception("Searching with empty string as arguments"));
 
         }
 
-        [Test]
-        public void Selected_operation_is_initially_empty_operation()
+        public class Tests : OperationStepViewModelTests
         {
-            Assert.AreEqual(typeof(EmptyOperationViewModel),_operationStepViewModel.SelectedOperation.GetType());
+            [Test]
+            public void Search_result_is_initially_not_available()
+            {
+                Assert.AreEqual(false, _operationStepViewModel.IsSearchResultVisible);
+            }
+
+            [Test]
+            public void Selected_operation_is_initially_empty_operation()
+            {
+                Assert.AreEqual(typeof(EmptyOperationViewModel), _operationStepViewModel.SelectedOperation.GetType());
+            }
+
+
+            [Test]
+            public async Task When_search_text_is_changed_then_a_new_search_query_is_made_and_the_old_is_cancelled()
+            {
+                var searchText = "ss";
+                var updatedSearchText = "sss";
+
+                var asyncCompletionInitialToken = MockGetRecordAsync(_safe, searchText, 1000, new List<RecordHeader>());
+                var asyncCompletionUpdatedToken = MockGetRecordAsync(_safe, updatedSearchText, 1000, new List<RecordHeader>());
+
+                _operationStepViewModel.SearchText = searchText;
+                _operationStepViewModel.SearchText = updatedSearchText;
+
+                await asyncCompletionUpdatedToken.WaitForTaskCompletion(10000);
+                Assert.False(asyncCompletionInitialToken.IsTaskCompleted);
+            }
+
+            [Test]
+            public void Operantion_modification_possibility()
+            {
+                Assert.AreEqual(true, _operationStepViewModel.IsOperationsChangingPossible);
+            }
         }
 
 
-        [Test]
-        public async Task When_search_is_completed_the_search_results_are_updated()
+
+        public class SearchTextAndSearchResult
         {
-            Semaphore semaphore = new Semaphore(0,1);
-            var searchText = "ss";
-            var headers = new List<RecordHeader>()
+            public string SearchText { get; set; }
+            public List<RecordHeader> SearchResults { get; set; }
+
+            public int TimeTakenForSearching { get; set; }
+        }
+
+
+        private static SearchTextAndSearchResult simpleSearchTextAndResult1 = new SearchTextAndSearchResult
+        {
+            SearchText = "ss",
+            SearchResults = new List<RecordHeader>()
             {
                 new RecordHeader() {Name = "1", Tags = new List<string>()},
                 new RecordHeader() {Name = "2", Tags = new List<string>()}
-            };
-            var asyncCompletionToken = MockGetRecordAsync(_safe, searchText, 100,  headers );
-            SetSearchText(searchText);
-            await asyncCompletionToken.WaitForTaskCompletion(10000);
+            },
+            TimeTakenForSearching = 100,
+        };
 
-            var actualHeaders = _searchResultPropertyObserver.PropertyValue.Select(x => x.RecordHeader).ToList();
+        
 
-            CollectionAssert.AreEqual(headers, actualHeaders);
-            Assert.AreEqual(true, _searchResultVisibilityObserver.PropertyValue);
+        private AsyncCompletionToken MockGetRecordAsync(SearchTextAndSearchResult searchTextAndSearchResult)
+        {
+            var asyncCompletionToken = MockGetRecordAsync(_safe, searchTextAndSearchResult.SearchText,
+                searchTextAndSearchResult.TimeTakenForSearching, searchTextAndSearchResult.SearchResults);
+            return asyncCompletionToken;
         }
 
         private void SetSearchText(string searchText)
@@ -88,152 +129,121 @@ namespace SafeViewModelTests
             _operationStepViewModel.SearchText = searchText;
         }
 
-        [Test]
-        public async Task When_search_is_result_is_selected_then_search_result_is_displayed_in_detail()
+
+        public class SearchTextEnteredAndSearchResultsAreCorrect : OperationStepViewModelTests
         {
-            Semaphore semaphore = new Semaphore(0, 1);
-            var searchText = "ss";
-            var headers = new List<RecordHeader>()
+            [SetUp]
+            public void SearchTextEnteredAndSearchResultsAreCorrectSetUp()
             {
-                new RecordHeader() {Name = "1", Tags = new List<string>()},
-                new RecordHeader() {Name = "2", Tags = new List<string>()}
-            };
-            var asyncCompletionToken = MockGetRecordAsync(_safe, searchText, 100, headers);
-            SetSearchText(searchText);
-            
-            await asyncCompletionToken.WaitForTaskCompletion(10000);
+                var asyncCompletionToken = MockGetRecordAsync(simpleSearchTextAndResult1);
+                SetSearchText(simpleSearchTextAndResult1.SearchText);
+                WaitForSearchResultsToBeAvailable(asyncCompletionToken);
+                Assume.That(_searchResultVisibilityObserver.PropertyValue, "The search results are invisible");
+                var actualHeaders = _searchResultPropertyObserver.PropertyValue.Select(x => x.RecordHeader).ToList();
+                Assume.That(simpleSearchTextAndResult1.SearchResults.Count == actualHeaders.Count, "The search results are wrong (count)");
+                var isAllSearchResultsListed =  actualHeaders.All(x => simpleSearchTextAndResult1.SearchResults.Contains(x));
+                Assume.That(isAllSearchResultsListed,"The search results are wrong");
+            }
 
-            Assume.That(_operationStepViewModel.SearchResults.Count == headers.Count);
-
-            
-            
-
-            _operationStepViewModel.SearchResults.ElementAt(1).SelectCommand.Execute();
-
-            Assert.AreEqual(typeof(RecordAlteringOperationViewModel),_selectedOperationPropertyObserver.PropertyValue.GetType());
-            Assert.AreEqual(1,_selectedOperationPropertyObserver.NumberOfTimesPropertyChanged);
-
-
-            Assert.False(_searchResultVisibilityObserver.PropertyValue);
-            Assert.True(_searchResultVisibilityObserver.NumberOfTimesPropertyChanged >= 0);
-
-            Assert.AreEqual(string.Empty, _searchTextPropertyObserver.PropertyValue);
-
-        }
-
-        [Test]
-        public async Task When_search_is_result_is_available_and_higlight_command_is_made_then_search_results_are_removed_and_search_text_is_removed()
-        {
-            var searchText = "ss";
-            var headers = new List<RecordHeader>()
+            private static void WaitForSearchResultsToBeAvailable(AsyncCompletionToken asyncCompletionToken)
             {
-                new RecordHeader() {Name = "1", Tags = new List<string>()},
-                new RecordHeader() {Name = "2", Tags = new List<string>()}
-            };
-            var searchText1 = String.Empty;
-            var headers1 = new List<RecordHeader>()
+                asyncCompletionToken.WaitForTaskCompletion(10000).Wait();
+            }
+
+
+
+            [Test]
+            public void When_search_result_is_selected_then_search_result_is_displayed_in_detail_and_search_text_is_reset()
             {
-            };
+                _operationStepViewModel.SearchResults.ElementAt(1).SelectCommand.Execute();
 
-            var asyncCompletionToken = MockGetRecordAsync(_safe, searchText, 100, headers);
-            MockGetRecordAsync(_safe, searchText1, 0, headers1);
-            _operationStepViewModel.SearchText = searchText;
-            await asyncCompletionToken.WaitForTaskCompletion(10000);
-            Assume.That(_operationStepViewModel.SearchResults.Count == headers.Count);
-            Assume.That(_operationStepViewModel.SelectedOperation.HighlightCommand.CanExecute());
-            _operationStepViewModel.SelectedOperation.HighlightCommand.Execute();
-            Assert.False(_searchResultVisibilityObserver.PropertyValue);
-            Assert.AreEqual(false,_operationStepViewModel.IsSearchResultVisible);
-            Assert.AreEqual(string.Empty,_operationStepViewModel.SearchText);
-        }
+                Assert.AreEqual(typeof(RecordAlteringOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
+                Assert.AreEqual(1, _selectedOperationPropertyObserver.NumberOfTimesPropertyChanged);
 
 
-        [Test]
-        public void When_add_command_is_made_then_selected_operation_is_add_operation()
-        {
-            Assume.That(_operationStepViewModel.AddCommand.CanExecute());
-            _operationStepViewModel.AddCommand.Execute();
-            Assert.AreEqual(typeof(AddOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
-        }
+                Assert.False(_searchResultVisibilityObserver.PropertyValue);
+                Assert.True(_searchResultVisibilityObserver.NumberOfTimesPropertyChanged >= 0);
 
-        [Test]
-        public async Task When_search_is_made_and_add_command_is_made_then_selected_operation_is_add_operation_and_search_result_is_invisible_and_search_text_is_false()
-        {
-            var searchText = "ss";
-            var headers = new List<RecordHeader>()
+                Assert.AreEqual(string.Empty, _searchTextPropertyObserver.PropertyValue);
+
+            }
+
+            [Test]
+            public void When_search_is_result_is_available_and_higlight_command_is_made_then_search_results_are_removed_and_search_text_is_removed()
             {
-                new RecordHeader() {Name = "1", Tags = new List<string>()},
-                new RecordHeader() {Name = "2", Tags = new List<string>()}
-            };
-            var asyncCompletionToken = MockGetRecordAsync(_safe, searchText, 100, headers);
-            SetSearchText(searchText);
-            await asyncCompletionToken.WaitForTaskCompletion(10000);
+                Assume.That(_operationStepViewModel.SelectedOperation.HighlightCommand.CanExecute());
+                _operationStepViewModel.SelectedOperation.HighlightCommand.Execute();
+                Assert.False(_searchResultVisibilityObserver.PropertyValue);
+                Assert.AreEqual(false, _operationStepViewModel.IsSearchResultVisible);
+                Assert.AreEqual(string.Empty, _operationStepViewModel.SearchText);
+            }
 
-            Assume.That(_operationStepViewModel.AddCommand.CanExecute());
-            _operationStepViewModel.AddCommand.Execute();
-            Assert.AreEqual(typeof(AddOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
-            Assert.False(_searchResultVisibilityObserver.PropertyValue);
-            Assert.AreEqual(string.Empty,_searchTextPropertyObserver.PropertyValue);
+
+            [Test]
+            public void When_search_is_made_and_add_command_is_made_then_selected_operation_is_add_operation_and_search_result_is_invisible_and_search_text_is_false()
+            {
+                Assume.That(_operationStepViewModel.AddCommand.CanExecute());
+                _operationStepViewModel.AddCommand.Execute();
+                Assert.AreEqual(typeof(AddOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
+                Assert.False(_searchResultVisibilityObserver.PropertyValue);
+                Assert.AreEqual(string.Empty, _searchTextPropertyObserver.PropertyValue);
+            }
         }
 
-        [Test]
-        public void When_add_command_is_made_and_discarded_then_selected_operation_is_empty_operation_and_operation_changing_is_possible()
+
+        public class InitiatedAddingARecord : OperationStepViewModelTests
         {
-            Assume.That(_operationStepViewModel.AddCommand.CanExecute());
-            _operationStepViewModel.AddCommand.Execute();
-            var addOperationViewModel = _selectedOperationPropertyObserver.PropertyValue as AddOperationViewModel;
-            Assume.That(addOperationViewModel != null);
-            Assert.True(addOperationViewModel.DiscardCommand.CanExecute());
-            addOperationViewModel.DiscardCommand.Execute();
-            Assert.AreEqual(typeof(EmptyOperationViewModel),_selectedOperationPropertyObserver.PropertyValue.GetType());
-            Assert.True(_operationChangingPossibilityObserver.PropertyValue);
+            private AddOperationViewModel _addOperationViewModel;
+
+            [SetUp]
+            public void InitiatedAddingARecordSetUp()
+            {
+                Assume.That(_operationStepViewModel.AddCommand.CanExecute());
+                _operationStepViewModel.AddCommand.Execute();
+                Assume.That(typeof(AddOperationViewModel) == _selectedOperationPropertyObserver.PropertyValue.GetType());
+                _addOperationViewModel = _operationStepViewModel.SelectedOperation as AddOperationViewModel;
+                Assume.That(_addOperationViewModel != null);
+            }
+
+            [Test]
+            public void When_record_is_discarded_then_selected_operation_is_empty_operation_and_operation_changing_is_possible()
+            {
+                Assert.True(_addOperationViewModel.DiscardCommand.CanExecute());
+                _addOperationViewModel.DiscardCommand.Execute();
+                Assert.AreEqual(typeof(EmptyOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
+                Assert.True(_operationChangingPossibilityObserver.PropertyValue);
+            }
+
+            [Test]
+            public void When_add_command_is_made_and_saved_then_selected_operation_is_modification_operation_and_operation_changing_is_possible()
+            {
+                Assert.True(_addOperationViewModel.SaveCommand.CanExecute());
+                _addOperationViewModel.SaveCommand.Execute();
+                Assert.AreEqual(typeof(RecordAlteringOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
+                Assert.True(_operationChangingPossibilityObserver.PropertyValue);
+            }
+
+
+
+
+            [Test]
+            public void When_add_command_is_made_then_operation_changing_is_disabled()
+            {
+                Assert.NotZero(_operationChangingPossibilityObserver.NumberOfTimesPropertyChanged);
+                Assert.False(_operationChangingPossibilityObserver.PropertyValue);
+            }
+
+
         }
 
-        [Test]
-        public void When_add_command_is_made_and_saved_then_selected_operation_is_modification_operation_and_operation_changing_is_possible()
-        {
-            Assume.That(_operationStepViewModel.AddCommand.CanExecute());
-            _operationStepViewModel.AddCommand.Execute();
-            var addOperationViewModel = _selectedOperationPropertyObserver.PropertyValue as AddOperationViewModel;
-            Assume.That(addOperationViewModel != null);
-            Assert.True(addOperationViewModel.SaveCommand.CanExecute());
-            addOperationViewModel.SaveCommand.Execute();
-            Assert.AreEqual(typeof(RecordAlteringOperationViewModel), _selectedOperationPropertyObserver.PropertyValue.GetType());
-            Assert.True(_operationChangingPossibilityObserver.PropertyValue);
-        }
 
-
-
-
-        [Test]
-        public void When_add_command_is_made_then_operation_changing_is_disabled()
-        {
-            Assume.That(_operationStepViewModel.AddCommand.CanExecute());
-            _operationStepViewModel.AddCommand.Execute();
-            Assert.NotZero(_operationChangingPossibilityObserver.NumberOfTimesPropertyChanged);
-            Assert.False(_operationChangingPossibilityObserver.PropertyValue);
-        }
 
         //Clicking of the SelectedResult should clear Search
         //Search Result should be closable
         //Search Result should disable Search
         //Add new result
 
-        [Test]
-        public async Task When_search_text_is_changed_then_a_new_search_query_is_made_and_the_old_is_cancelled()
-        {
-            var searchText = "ss";
-            var updatedSearchText = "sss";
 
-            var asyncCompletionInitialToken = MockGetRecordAsync(_safe, searchText, 1000, new List<RecordHeader>());
-            var asyncCompletionUpdatedToken = MockGetRecordAsync(_safe, updatedSearchText, 1000, new List<RecordHeader>());
-            
-            _operationStepViewModel.SearchText = searchText;
-            _operationStepViewModel.SearchText = updatedSearchText;
-
-            await asyncCompletionUpdatedToken.WaitForTaskCompletion(10000);
-            Assert.False(asyncCompletionInitialToken.IsTaskCompleted);
-        }
 
         private AsyncCompletionToken MockGetRecordAsync(ISafe safe, string searchText, int millisecondsTimeout, List<RecordHeader> recordHeaders)
         {
@@ -269,10 +279,6 @@ namespace SafeViewModelTests
             public bool IsTaskCompleted { get; set; }
         }
 
-        [Test]
-        public void Operantion_modification_possibility()
-        {
-            Assert.AreEqual(true,_operationStepViewModel.IsOperationsChangingPossible);
-        }
+
     }
 }
