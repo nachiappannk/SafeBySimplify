acb46a40399c303578e8b90e18cd9fd72f91bf44
Nachiappan <nachiapan@gmail.com>
2017-12-25 10:34:01 +0530

    Cleaned Up SignInViewModel

--- SafeBySimplify/SafeViewModel/SignInViewModel.cs
+++ SafeBySimplify/SafeViewModel/SignInViewModel.cs
@@ -1,5 +1,28 @@
-﻿using System;
-using System.Collections;
+﻿/*
+MIT License
+
+Copyright(c) 2017 Nachiappan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Prism.Commands;
@@ -9,8 +32,58 @@ namespace SafeViewModel
 {
     public class SignInViewModel : NotifiesPropertyChanged
     {
+        public const string WrongPasswordErrorMessage = "Wrong Password";
+
         private readonly ISafeProviderForExistingUser _safeProviderForExistingUser;
         private readonly Action<ISafe, string> _signUpCompletionCallback;
+
+        public DelegateCommand SignInCommand { get; set; }
+        public List<string> AvailableUserNames { get; set; }
+        public bool IsEnabled { get; set; }
+
+        private string _signInUserName;
+        public string SignInUserName
+        {
+            get { return _signInUserName; }
+            set
+            {
+                if (_signInUserName != value)
+                {
+                    _signInUserName = value;
+                    ErrorMessage = string.Empty;
+                    SignInPassword = String.Empty;
+                }
+            }
+        }
+
+        private string _signInPassword = String.Empty;
+        public string SignInPassword
+        {
+            get { return _signInPassword; }
+            set
+            {
+                if (_signInPassword != value)
+                {
+                    _signInPassword = value;
+                    ErrorMessage = string.Empty;
+                    SignInCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                if (_errorMessage != value)
+                {
+                    _errorMessage = value;
+                    FirePropertyChanged();
+                }
+            }
+        }
 
         public SignInViewModel(ISafeProviderForExistingUser safeProviderForExistingUser,
             Action<ISafe, string> signUpCompletionCallback)
@@ -21,7 +94,6 @@ namespace SafeViewModel
             AvailableUserNames = _safeProviderForExistingUser.GetUserNames().ToList();
             IsEnabled = AvailableUserNames.Count != 0;
             if(IsEnabled) _signInUserName = AvailableUserNames.ElementAt(0);
-            _signInPassword = string.Empty;
             SignInCommand = new DelegateCommand(SignIn, CanSignIn);
         }
 
@@ -41,57 +113,6 @@ namespace SafeViewModel
         private bool CanSignIn()
         {
             return SignInPassword.Length != 0;
-        }
-
-        private string _signInUserName;
-
-        public string SignInUserName
-        {
-            get { return _signInUserName; }
-            set
-            {
-                if (_signInUserName != value)
-                {
-                    _signInUserName = value;
-                    ErrorMessage = string.Empty;
-                    SignInPassword = String.Empty;
-                }
-            }
-        }
-
-        private string _signInPassword;
-        public const string WrongPasswordErrorMessage = "Wrong Password";
-
-        public string SignInPassword
-        {
-            get { return _signInPassword; }
-            set
-            {
-                if (_signInPassword != value)
-                {
-                    _signInPassword = value;
-                    ErrorMessage = string.Empty;
-                    SignInCommand.RaiseCanExecuteChanged();
-                }
-            }
-        }
-        public DelegateCommand SignInCommand { get; set; }
-        public List<string> AvailableUserNames { get; set; }
-        public bool IsEnabled { get; set; }
-
-        private string _errorMessage;
-
-        public string ErrorMessage
-        {
-            get { return _errorMessage; }
-            set
-            {
-                if (_errorMessage != value)
-                {
-                    _errorMessage = value;
-                    FirePropertyChanged();
-                }
-            }
         }
     }
 }
