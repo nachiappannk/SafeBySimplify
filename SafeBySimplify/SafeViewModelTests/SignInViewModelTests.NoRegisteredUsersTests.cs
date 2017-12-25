3b7b119079831b79ed37fca3389fdbfc55e770b1
Nachiappan <nachiapan@gmail.com>
2017-12-25 10:54:18 +0530

    Cleaned Up Sign In View Model Tests

--- SafeBySimplify/SafeViewModelTests/SignInViewModelTests.NoRegisteredUsersTests.cs
+++ SafeBySimplify/SafeViewModelTests/SignInViewModelTests.NoRegisteredUsersTests.cs
@@ -1,4 +1,28 @@
-﻿using System.Collections.Generic;
+﻿/*
+MIT License
+
+Copyright(c) 2017 Nachiappan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Collections.Generic;
 using NSubstitute;
 using NUnit.Framework;
 using SafeViewModel;
@@ -13,16 +37,15 @@ namespace SafeViewModelTests
             public void Setup()
             {
                 _safeProviderForExistingUser.GetUserNames().Returns(new List<string>());
-                _signInViewModel = new SignInViewModel(_safeProviderForExistingUser, 
+                SignInViewModel = new SignInViewModel(_safeProviderForExistingUser, 
                     (s, n) => { });
             }
 
             [Test]
             public void When_there_are_no_user_accounts_then_sign_in_is_disabled()
             {
-                Assert.AreEqual(false, _signInViewModel.IsEnabled);
+                Assert.AreEqual(false, SignInViewModel.IsEnabled);
             }
-
         }
     }
 }
--- SafeBySimplify/SafeViewModelTests/SignInViewModelTests.RegisteredUserAvailableTests.cs
+++ SafeBySimplify/SafeViewModelTests/SignInViewModelTests.RegisteredUserAvailableTests.cs
@@ -1,8 +1,31 @@
-﻿using System.Collections.Generic;
+﻿/*
+MIT License
+
+Copyright(c) 2017 Nachiappan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Collections.Generic;
 using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
-using SafeModel;
 using SafeViewModel;
 using SafeViewModelTests.TestTools;
 
@@ -10,59 +33,59 @@ namespace SafeViewModelTests
 {
     public partial class SignInViewModelTests
     {
-        public partial class RegisteredUserAvailableTests : SignInViewModelTests
+        public class RegisteredUserAvailableTests : SignInViewModelTests
         {
             private List<string> _registeredUserNames;
             private CommandObserver _commandObserver;
             private ViewModelPropertyObserver<string> _errorMessagePropertyObserver;
 
             [SetUp]
-            public void SetUp()
+            public void RegisteredUserAvailableTestsSetUp()
             {
                 _registeredUserNames = new List<string> {"UserName1", "UserName2", "UserName3",};
                 _safeProviderForExistingUser.GetUserNames().Returns(_registeredUserNames);
-                _signInViewModel = new SignInViewModel(_safeProviderForExistingUser, (safe, n) => { });
-                _commandObserver = _signInViewModel.SignInCommand.GetDelegateCommandObserver();
-                _errorMessagePropertyObserver = _signInViewModel.GetPropertyObserver<string>(nameof(_signInViewModel.ErrorMessage));
+                SignInViewModel = new SignInViewModel(_safeProviderForExistingUser, (safe, n) => { });
+                _commandObserver = SignInViewModel.SignInCommand.GetDelegateCommandObserver();
+                _errorMessagePropertyObserver = SignInViewModel.GetPropertyObserver<string>(nameof(SignInViewModel.ErrorMessage));
 
             }
 
             [Test]
             public void When_there_are_user_accounts_then_sign_in_is_enabled()
             {
-                Assert.AreEqual(true, _signInViewModel.IsEnabled);
+                Assert.AreEqual(true, SignInViewModel.IsEnabled);
             }
 
             [Test]
             public void When_there_are_user_accounts_then_same_usernames_are_available_for_login()
             {
-                CollectionAssert.AreEquivalent(_registeredUserNames, _signInViewModel.AvailableUserNames);
+                CollectionAssert.AreEquivalent(_registeredUserNames, SignInViewModel.AvailableUserNames);
             }
 
             [Test]
             public void When_there_are_user_accounts_then_first_user_name_is_available_for_login()
             {
-                Assert.AreEqual(_registeredUserNames.ElementAt(0), _signInViewModel.SignInUserName);
+                Assert.AreEqual(_registeredUserNames.ElementAt(0), SignInViewModel.SignInUserName);
             }
 
             [Test]
             public void At_the_begining_password_is_empty()
             {
-                Assert.AreEqual(string.Empty, _signInViewModel.SignInPassword);
+                Assert.AreEqual(string.Empty, SignInViewModel.SignInPassword);
             }
 
             [Test]
             public void When_password_is_entered_then_sign_in_command_is_enabled()
             {
-                _signInViewModel.SignInPassword = "SomePassword";
+                SignInViewModel.SignInPassword = "SomePassword";
                 Assert.True(_commandObserver.ValueOfCanExecuteOnLatestEvent);
             }
 
             [Test]
             public void When_password_is_removed_then_sign_in_command_is_disabled()
             {
-                _signInViewModel.SignInPassword = "SomePassword";
-                _signInViewModel.SignInPassword = "";
+                SignInViewModel.SignInPassword = "SomePassword";
+                SignInViewModel.SignInPassword = "";
                 Assert.False(_commandObserver.ValueOfCanExecuteOnLatestEvent);
             }
 
@@ -77,10 +100,10 @@ namespace SafeViewModelTests
             [Test]
             public void When_user_name_is_modified_password_is_cleared()
             {
-                _signInViewModel.SignInPassword = "Password";
-                _signInViewModel.SignInUserName = _signInViewModel.AvailableUserNames.ElementAt(1);
+                SignInViewModel.SignInPassword = "Password";
+                SignInViewModel.SignInUserName = SignInViewModel.AvailableUserNames.ElementAt(1);
 
-                Assert.AreEqual(string.Empty,_signInViewModel.SignInPassword);
+                Assert.AreEqual(string.Empty,SignInViewModel.SignInPassword);
             }
 
             [Test]
@@ -89,7 +112,7 @@ namespace SafeViewModelTests
                 SignInWithPassword("WrongPassword");
 
                 Assume.That(SignInViewModel.WrongPasswordErrorMessage == _errorMessagePropertyObserver.PropertyValue);
-                _signInViewModel.SignInPassword = "SomeOtherPassword";
+                SignInViewModel.SignInPassword = "SomeOtherPassword";
                 Assert.AreEqual(string.Empty, _errorMessagePropertyObserver.PropertyValue);
             }
 
@@ -99,15 +122,15 @@ namespace SafeViewModelTests
                 SignInWithPassword("WrongPassword");
 
                 Assume.That(SignInViewModel.WrongPasswordErrorMessage == _errorMessagePropertyObserver.PropertyValue);
-                _signInViewModel.SignInUserName = _signInViewModel.AvailableUserNames.ElementAt(1);
+                SignInViewModel.SignInUserName = SignInViewModel.AvailableUserNames.ElementAt(1);
                 Assert.AreEqual(string.Empty, _errorMessagePropertyObserver.PropertyValue);
             }
 
             private void SignInWithPassword(string signInPassword)
             {
-                _signInViewModel.SignInPassword = signInPassword;
-                Assume.That(_signInViewModel.SignInCommand.CanExecute());
-                _signInViewModel.SignInCommand.Execute();
+                SignInViewModel.SignInPassword = signInPassword;
+                Assume.That(SignInViewModel.SignInCommand.CanExecute());
+                SignInViewModel.SignInCommand.Execute();
             }
         }
     }
