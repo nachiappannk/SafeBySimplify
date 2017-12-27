4b426e4d51d355eab0fa364fa1bc2e668c7dc049
Nachiappan <nachiapan@gmail.com>
2017-12-28 00:16:43 +0530

    Preparing for the pre release

--- SafeBySimplify/SafeBySimplify/Properties/AssemblyInfo.cs
+++ SafeBySimplify/SafeBySimplify/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("SafeBySimplify")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Licence Owner")]
+[assembly: AssemblyCompany("Nachiappan")]
 [assembly: AssemblyProduct("SafeBySimplify")]
-[assembly: AssemblyCopyright("Copyright © Licence Owner 2017")]
+[assembly: AssemblyCopyright("Copyright © Nachiappan 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
